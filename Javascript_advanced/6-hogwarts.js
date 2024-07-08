c58389b6cdc80080b844eca765cf1070a0873582
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-07-08 16:14:36 -0500

    The "return an object" line from the task was bugging me. I figured it out.

--- Javascript_advanced/6-hogwarts.js
+++ Javascript_advanced/6-hogwarts.js
@@ -1,39 +1,36 @@
-class studentHogwarts {
+class StudentHogwarts {
   #privateScore = 0;
   #name = null;
 
+  constructor() {
+    // Return an "object" as the task says and each key has the related function
+    return {
+      setName: (newName) => { this.#name = newName; },
+      rewardStudent: () => this.#changeScoreBy(1),
+      penalizeStudent: () => this.#changeScoreBy(-1),
+      getScore: () => {
+        return `${this.#name}: ${this.#privateScore}`;
+      }
+    };
+  }
+  // Class does not give access to this method and it is private
   #changeScoreBy(points) {
     this.#privateScore += points;
-  }
-
-  setName(name) {
-    this.#name = name;
-  }
-
-  rewardStudent() {
-    this.#changeScoreBy(1);
-  }
-
-  penalizeStudent() {
-    this.#changeScoreBy(-1);
-  }
-
-  getScore() {
-    return `${this.#name}: ${this.#privateScore}`;
   }
 }
 
 // Harry
-const harry = new studentHogwarts();
+const harry = new StudentHogwarts();
 harry.setName("Harry");
 harry.rewardStudent();
 harry.rewardStudent();
 harry.rewardStudent();
 harry.rewardStudent();
 console.log(harry.getScore());
+console.log(harry)
 
 // Draco
-const draco = new studentHogwarts();
+const draco = new StudentHogwarts();
 draco.setName("Draco");
 draco.rewardStudent();
 draco.penalizeStudent();
