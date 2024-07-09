fdf75ddd09b527da783375df430a158906d81439
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-07-09 12:10:00 -0500

    Done with 10

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,24 @@
+function countPrimeNumbers() {
+  let primeCount = 0;
+
+  for (let num = 2; num <= 100; num++) {
+    let isPrime = true;
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primeCount++
+    }
+  }
+  return primeCount;
+}
+
+let start = performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+let end = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds.`);
